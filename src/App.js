50b5df5819442b336c03cf33554d1f24864a3ed5
Cal-Ell <calvin.malagon@ultimagen.com>
2022-01-10 18:13:13 -0800

    assignment1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Assignment from './Assignment 1/Assignment';
+
+function App() {
+  return (
+    <div className="App">
+      <Assignment />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Assignment 1/Assignment.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './assignment.css'
+
+const Assignment = () => {
+    return (
+        <div class="container">
+           
+        <div class="container_4">
+            <div class="whole_bottle">
+                <div class="top"></div>
+                <div class="bottom"></div>
+            </div>
+            <div class="whole_bottle">
+                <div class="top"></div>
+                <div class="bottom"></div>
+            </div>
+            <div class="whole_bottle">
+                <div class="top"></div>
+                <div class="bottom"></div>
+            </div>
+            <div class="whole_bottle">
+                <div class="top"></div>
+                <div class="bottom"></div>
+            </div>
+            <div class="whole_bottle">
+                <div class="top"></div>
+                <div class="bottom"></div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Assignment
