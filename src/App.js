7e1fa56a6cd214f67ead61a69f6dadb9498a00bb
Cal-Ell <calvin.malagon@ultimagen.com>
2022-01-16 17:57:19 -0800

    space

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Assignment from './Assignment 1/Assignment';
+import Assignment2 from './Assignment 2/Assignment2';
 
 function App() {
   return (
     <div className="App">
+      
       <Assignment />
+      <Assignment2 />
     </div>
   );
 }
